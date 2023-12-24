384ccee23945294a88af4d517f0b88653403d208
Doug Murphy <doug.murphy82@gmail.com>
2023-12-23 21:57:31 -0500

    Scaffold project for new mod.

--- GuysNight.LethalCompanyMod.ReasonableWeights/Plugin.cs
+++ GuysNight.LethalCompanyMod.ReasonableWeights/Plugin.cs
@@ -1,14 +1,16 @@
-﻿using BepInEx;
+﻿using System.Reflection;
+using BepInEx;
+using HarmonyLib;
 
-namespace DougMurphy.LethalCompanyMod.NoLostLoot
+namespace GuysNight.LethalCompanyMod.ReasonableWeights
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
         {
-            // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            new Harmony(PluginInfo.PLUGIN_GUID).PatchAll(Assembly.GetExecutingAssembly());
         }
     }
 }
