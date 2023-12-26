9d0552007ca000054240125c323af0c2eed76ce5
Doug Murphy <doug.murphy82@gmail.com>
2023-12-26 13:12:57 -0500

    Create initial set of item overrides.

---
+++ GuysNight.LethalCompanyMod.ReasonableWeights/ItemOverridesContainer.cs
@@ -0,0 +1,25 @@
+﻿using GuysNight.LethalCompanyMod.ReasonableWeights.Models;
+
+namespace GuysNight.LethalCompanyMod.ReasonableWeights {
+	public static class ItemOverridesContainer {
+		public static ItemPropertyOverride[] ItemOverrides { get; } = {
+			//spawned loot items
+			new ItemPropertyOverride("Key", 0),
+
+			//purchased items
+			new ItemPropertyOverride("BoomboxWeight", 5),
+			new ItemPropertyOverride("ExtensionLadderWeight", 5),
+			new ItemPropertyOverride("FlashlightWeight", 2),
+			new ItemPropertyOverride("JetpackWeight", 25),
+			new ItemPropertyOverride("LockPickerWeight", 2),
+			new ItemPropertyOverride("ProFlashlightWeight", 2),
+			new ItemPropertyOverride("RadarBoosterWeight", 5),
+			new ItemPropertyOverride("ShovelWeight", 5),
+			new ItemPropertyOverride("SprayPaintWeight", 1),
+			new ItemPropertyOverride("StunGrenadeWeight", 2),
+			new ItemPropertyOverride("TZPInhalantWeight", 2),
+			new ItemPropertyOverride("WalkieTalkieWeight", 2),
+			new ItemPropertyOverride("ZapGunWeight", 4)
+		};
+	}
+}
---
+++ GuysNight.LethalCompanyMod.ReasonableWeights/Models/ItemPropertyOverride.cs
@@ -0,0 +1,25 @@
+﻿namespace GuysNight.LethalCompanyMod.ReasonableWeights.Models {
+	/// <summary>
+	/// Contains the data required for overriding various aspects of an item.
+	/// </summary>
+	public sealed class ItemPropertyOverride {
+		public ItemPropertyOverride(string name, float weight) {
+			Name = name;
+			Weight = weight;
+		}
+
+		/// <summary>
+		/// The name of the item to override.
+		/// </summary>
+		public string Name { get; }
+
+		/// <summary>
+		/// The weight to set for the item.
+		/// </summary>
+		public float Weight { get; }
+
+		public override string ToString() {
+			return $"Name: {Name}; Weight: {Weight};";
+		}
+	}
+}
