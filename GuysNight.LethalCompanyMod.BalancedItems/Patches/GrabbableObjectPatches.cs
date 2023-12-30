dd3efb7902efc37d769dbf9e17cf50c07354ba95
Doug Murphy <doug.murphy82@gmail.com>
2023-12-29 20:31:01 -0500

    Move scrap override to RoundManager.
    * GrabbableObject is too late! It won't override the scrap for the initial spawn - only subsequent spawns.

--- GuysNight.LethalCompanyMod.BalancedItems/Patches/GrabbableObjectPatches.cs
+++ GuysNight.LethalCompanyMod.BalancedItems/Patches/GrabbableObjectPatches.cs
@@ -19,37 +19,26 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Patches {
 
 			SharedComponents.Logger.LogInfo($"item.itemProperties.name is '{__instance.itemProperties.name}'");
 			SharedComponents.Logger.LogInfo($"item.itemProperties.weight is '{NumericUtilities.DenormalizeWeight(__instance.itemProperties.weight)}'");
-			SharedComponents.Logger.LogInfo($"item.itemProperties.minValue is '{__instance.itemProperties.minValue}'");
-			SharedComponents.Logger.LogInfo($"item.itemProperties.maxValue is '{__instance.itemProperties.maxValue}'");
 
 			var itemOverride = Array.Find(ItemOverridesContainer.ItemOverrides, itemOverride => itemOverride.Name == __instance.itemProperties.name);
 			if (itemOverride is null)
 			{
-				SharedComponents.Logger.LogInfo("Unable to find item to override. Making no changes.");
+				SharedComponents.Logger.LogInfo("No override exists for this item. Making no changes.");
 
 				return;
 			}
 
-			if (itemOverride.MinValue.HasValue && itemOverride.MaxValue.HasValue)
+			if (!itemOverride.Weight.HasValue)
 			{
-				UpdateItemValue(__instance, itemOverride.MinValue.Value, itemOverride.MaxValue.Value);
+				SharedComponents.Logger.LogInfo("An item override was found, but it did not have a weight specified.");
+
+				return;
 			}
 
-			if (itemOverride.Weight.HasValue)
-			{
-				UpdateItemWeight(__instance, itemOverride.Weight.Value);
-			}
-		}
-
-		private static void UpdateItemValue(GrabbableObject item, int minValue, int maxValue) {
-			SharedComponents.Logger.LogInfo($"Existing sell value range for '{item.itemProperties.name}' is '{item.itemProperties.minValue}' - '{item.itemProperties.maxValue}'");
-			item.itemProperties.minValue = minValue;
-			item.itemProperties.maxValue = maxValue;
-			SharedComponents.Logger.LogInfo($"Successfully override sell value range for '{item.itemProperties.name}' to be '{minValue}' - '{maxValue}'");
+			UpdateItemWeight(__instance, itemOverride.Weight.Value);
 		}
 
 		private static void UpdateItemWeight(GrabbableObject item, float weight) {
-			SharedComponents.Logger.LogInfo($"Existing weight for '{item.itemProperties.name}' is '{NumericUtilities.DenormalizeWeight(item.itemProperties.weight)}'");
 			item.itemProperties.weight = weight;
 			SharedComponents.Logger.LogInfo($"Successfully overrode weight for '{item.itemProperties.name}' to be '{NumericUtilities.DenormalizeWeight(weight)}'");
 		}
---
+++ GuysNight.LethalCompanyMod.BalancedItems/Patches/RoundManagerPatches.cs
@@ -0,0 +1,48 @@
+﻿#pragma warning disable	S1118
+
+using System;
+using System.Linq;
+using GuysNight.LethalCompanyMod.BalancedItems.Models;
+using HarmonyLib;
+
+namespace GuysNight.LethalCompanyMod.BalancedItems.Patches {
+	[HarmonyPatch(typeof(RoundManager))]
+	public class RoundManagerPatches {
+		[HarmonyPrefix]
+		[HarmonyPatch("SpawnScrapInLevel")]
+		public static void ChangeScrapValues(RoundManager __instance) {
+			if (__instance == null) {
+				SharedComponents.Logger.LogInfo("__instance is null for some reason. Exiting override.");
+
+				return;
+			}
+
+			foreach (var spawnableScrap in __instance.currentLevel.spawnableScrap.Select(x => x.spawnableItem)) {
+				SharedComponents.Logger.LogInfo($"spawnableScrap.name is '{spawnableScrap.name}'");
+				SharedComponents.Logger.LogInfo($"spawnableScrap.minValue is '{spawnableScrap.minValue}'");
+				SharedComponents.Logger.LogInfo($"spawnableScrap.maxValue is '{spawnableScrap.maxValue}'");
+
+				var itemOverride = Array.Find(ItemOverridesContainer.ItemOverrides, itemOverride => itemOverride.Name == spawnableScrap.name);
+				if (itemOverride is null) {
+					SharedComponents.Logger.LogInfo("No override exists for this item. Making no changes.");
+
+					continue;
+				}
+
+				if (!itemOverride.MinValue.HasValue || !itemOverride.MaxValue.HasValue) {
+					SharedComponents.Logger.LogInfo(@$"An item override was found, but it did not have both a {nameof(ItemPropertyOverride.MinValue)} and {nameof(ItemPropertyOverride.MaxValue)} specified. {nameof(ItemPropertyOverride.MinValue)} = '{itemOverride.MinValue}' {nameof(ItemPropertyOverride.MaxValue)} = '{itemOverride.MaxValue}'");
+
+					continue;
+				}
+
+				UpdateItemValue(spawnableScrap, itemOverride.MinValue.Value, itemOverride.MaxValue.Value);
+			}
+		}
+
+		private static void UpdateItemValue(Item item, int minValue, int maxValue) {
+			item.minValue = minValue;
+			item.maxValue = maxValue;
+			SharedComponents.Logger.LogInfo($"Successfully override sell value range for '{item.name}' to be '{minValue}' - '{maxValue}'");
+		}
+	}
+}
