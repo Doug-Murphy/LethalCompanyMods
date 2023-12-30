2daa6333ecd655478fea20db60fcd19c86759e9f
Doug Murphy <doug.murphy82@gmail.com>
2023-12-29 22:04:52 -0500

    Rework value overrides to be a specified average price with a predefined spread.

--- GuysNight.LethalCompanyMod.BalancedItems/Models/ItemPropertyOverride.cs
+++ GuysNight.LethalCompanyMod.BalancedItems/Models/ItemPropertyOverride.cs
@@ -6,6 +6,7 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Models {
 	/// Contains the data required for overriding various aspects of an item.
 	/// </summary>
 	public sealed class ItemPropertyOverride {
+		private readonly ushort _averageValue;
 		private float? _weight;
 
 		/// <summary>
@@ -15,7 +16,7 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Models {
 		public ItemPropertyOverride(string name) {
 			Name = name;
 		}
-		
+
 		/// <summary>
 		/// The constructor to use when overriding only the item's weight is desired.
 		/// </summary>
@@ -23,10 +24,10 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Models {
 		/// <param name="weight">How much you want the item to weigh.</param>
 		/// <exception cref="ArgumentOutOfRangeException">Thrown when weight is negative.</exception>
 		public ItemPropertyOverride(string name, float weight) {
-			if (weight < 0)
-			{
+			if (weight < 0) {
 				throw new ArgumentOutOfRangeException(nameof(weight), "You cannot set a negative weight value.");
 			}
+
 			Name = name;
 			Weight = weight;
 		}
@@ -36,74 +37,47 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Models {
 		/// </summary>
 		/// <param name="name">The item name.</param>
 		/// <param name="weight">How much you want the item to weigh.</param>
-		/// <param name="minValue">How much you want the minimum sell value to be.</param>
-		/// <param name="maxValue">How much you want the maximum sell value to be.</param>
-		/// <exception cref="ArgumentException">Thrown when minValue is greater than maxValue.</exception>
-		/// <exception cref="ArgumentOutOfRangeException">Thrown when minValue is negative.</exception>
-		public ItemPropertyOverride(string name, float weight, int minValue, int maxValue) {
-			if (minValue < 0)
-			{
-				throw new ArgumentOutOfRangeException(nameof(minValue), "You cannot set a negative minimum sell value.");
-			}
-			if (minValue > maxValue)
-			{
-				throw new ArgumentException("The minimum value cannot be larger than the maximum value.", nameof(minValue));
-			}
-			
+		/// <param name="averageValue">The average price that you want the scrap item to be worth.</param>
+		public ItemPropertyOverride(string name, float weight, ushort averageValue) {
+			_averageValue = averageValue;
 			Name = name;
-			MinValue = minValue;
-			MaxValue = maxValue;
 			Weight = weight;
 		}
-		
+
 		/// <summary>
 		/// The constructor to use when overriding only the item's value is desired.
 		/// </summary>
 		/// <param name="name">The item name.</param>
-		/// <param name="minValue">How much you want the minimum sell value to be.</param>
-		/// <param name="maxValue">How much you want the maximum sell value to be.</param>
-		/// <exception cref="ArgumentException">Thrown when minValue is greater than maxValue.</exception>
-		/// <exception cref="ArgumentOutOfRangeException">Thrown when minValue is negative.</exception>
-		public ItemPropertyOverride(string name, int minValue, int maxValue) {
-			if (minValue < 0)
-			{
-				throw new ArgumentOutOfRangeException(nameof(minValue), "You cannot set a negative minimum sell value.");
-			}
-			if (minValue > maxValue)
-			{
-				throw new ArgumentException("The minimum value cannot be larger than the maximum value.", nameof(minValue));
-			}
-			
+		/// <param name="averageValue">The average price that you want the scrap item to be worth.</param>
+		public ItemPropertyOverride(string name, ushort averageValue) {
+			_averageValue = averageValue;
 			Name = name;
-			MinValue = minValue;
-			MaxValue = maxValue;
 		}
 
 		/// <summary>
 		/// The name of the item to override.
 		/// </summary>
 		public string Name { get; }
-		
+
 		/// <summary>
 		/// The minimum value that the item can sell for.
 		/// </summary>
-		public int? MinValue { get; }
-		
+		public int? MinValue => (int)Math.Round(_averageValue - _averageValue * .2, MidpointRounding.AwayFromZero);
+
 		/// <summary>
 		/// The maximum value that the item can sell for.
 		/// </summary>
-		public int? MaxValue { get; }
+		public int? MaxValue => (int)Math.Round(_averageValue + _averageValue * .2, MidpointRounding.AwayFromZero);
 
 		/// <summary>
-		/// The weight to set for the item. This value should be normalized to the game's representation.
+		/// The weight to set for the item. This value is normalized to the game's representation when setting.
 		/// </summary>
 		/// <remarks>If the desired weight is 5 pounds, the normalized representation is 1.05</remarks>
 		public float? Weight {
 			get => _weight;
 
 			private set {
-				if (value.HasValue)
-				{
+				if (value.HasValue) {
 					_weight = NumericUtilities.NormalizeWeight(value.Value);
 				}
 			}
