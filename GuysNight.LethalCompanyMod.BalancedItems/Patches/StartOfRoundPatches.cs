217f9ef0b16d7c021e42cb3161e0e1f76b82b520
Doug Murphy <doug.murphy82@gmail.com>
2024-01-01 19:13:24 -0500

    Create config entry and bind config values to overrides for scrap item weight and average sell value when starting a round.

--- GuysNight.LethalCompanyMod.BalancedItems/Patches/StartOfRoundPatches.cs
+++ GuysNight.LethalCompanyMod.BalancedItems/Patches/StartOfRoundPatches.cs
@@ -1,6 +1,7 @@
 ﻿#pragma warning disable S1118
 using System;
 using System.Linq;
+using GuysNight.LethalCompanyMod.BalancedItems.Models;
 using GuysNight.LethalCompanyMod.BalancedItems.Utilities;
 using HarmonyLib;
 
@@ -19,20 +20,37 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Patches {
 			SharedComponents.Logger.LogInfo($"Found {__instance.levels.Length} levels.");
 			foreach (var level in __instance.levels) {
 				foreach (var spawnableScrap in level.spawnableScrap.OrderBy(s => s.spawnableItem.name)) {
-					//add weight to config if it not already present
-					SharedComponents.ConfigFile.Bind(Constants.ConfigSectionHeaderWeight,
-						spawnableScrap.spawnableItem.name,
-						NumericUtilities.DenormalizeWeight(spawnableScrap.spawnableItem.weight),
-						string.Format(Constants.ConfigDescriptionWeight, spawnableScrap.spawnableItem.name)
-					);
-
-					//add sell value to config if it is not already present
-					SharedComponents.ConfigFile.Bind(Constants.ConfigSectionHeaderAverageSellValues,
-						spawnableScrap.spawnableItem.name,
-						(int)Math.Round(new[] { spawnableScrap.spawnableItem.minValue, spawnableScrap.spawnableItem.maxValue }.Average(), MidpointRounding.AwayFromZero),
-						string.Format(Constants.ConfigDescriptionAverageSellValues, spawnableScrap.spawnableItem.name));
-
 					SharedComponents.Logger.LogInfo($"On level '{level.name}' we found a spawnable scrap item named '{spawnableScrap.spawnableItem.name}' with weight '{NumericUtilities.DenormalizeWeight(spawnableScrap.spawnableItem.weight)}' pounds, rarity '{spawnableScrap.rarity}', min value '{spawnableScrap.spawnableItem.minValue}', and max value '{spawnableScrap.spawnableItem.maxValue}'");
+					SharedComponents.Logger.LogInfo($"Begin adding config entries and setting override values for '{spawnableScrap.spawnableItem.name}'");
+					
+					var itemWeight = spawnableScrap.spawnableItem.weight;
+					var itemMinValue = spawnableScrap.spawnableItem.minValue;
+					var itemMaxValue = spawnableScrap.spawnableItem.maxValue;
+					var itemAverageValue = (ushort)Math.Round(new[] { itemMinValue, itemMaxValue }.Average(), MidpointRounding.AwayFromZero);
+
+					//if the overrides container does not contain an override for the current scrap item, add an entry
+					if (!ItemOverridesContainer.ItemOverrides.ContainsKey(spawnableScrap.spawnableItem.name)) {
+						ItemOverridesContainer.ItemOverrides[spawnableScrap.spawnableItem.name] = new OverrideProperties();
+					}
+
+					var itemOverrides = ItemOverridesContainer.ItemOverrides[spawnableScrap.spawnableItem.name];
+
+					//if weight is not added in the config, add it for future
+					//if weight is added in the config, retrieve the value and set it in the overrides
+					itemOverrides.Weight = NumericUtilities.NormalizeWeight(SharedComponents.ConfigFile.Bind(Constants.ConfigSectionHeaderWeight,
+						spawnableScrap.spawnableItem.name,
+						NumericUtilities.DenormalizeWeight(itemWeight),
+						string.Format(Constants.ConfigDescriptionWeight, spawnableScrap.spawnableItem.name)
+					).Value);
+
+					//if sell value is not added in the config, add it for future
+					//if sell value is added in the config, retrieve the value and set it in the overrides
+					itemOverrides.AverageValue = SharedComponents.ConfigFile.Bind(Constants.ConfigSectionHeaderAverageSellValues,
+						spawnableScrap.spawnableItem.name,
+						itemAverageValue,
+						string.Format(Constants.ConfigDescriptionAverageSellValues, spawnableScrap.spawnableItem.name)).Value;
+					
+					SharedComponents.Logger.LogInfo($"Finish adding config entries and setting override values for '{spawnableScrap.spawnableItem.name}'");
 				}
 			}
 		}
