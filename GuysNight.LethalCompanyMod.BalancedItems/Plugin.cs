f221c0540b41df334799a3441638cfad3876314c
Doug Murphy <doug.murphy82@gmail.com>
2024-01-01 19:11:53 -0500

    No longer bind config values on mod load. We do it dyniamcally later.

--- GuysNight.LethalCompanyMod.BalancedItems/Plugin.cs
+++ GuysNight.LethalCompanyMod.BalancedItems/Plugin.cs
@@ -3,7 +3,6 @@ using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
 using GuysNight.LethalCompanyMod.BalancedItems.Extensions;
-using GuysNight.LethalCompanyMod.BalancedItems.Utilities;
 using HarmonyLib;
 
 namespace GuysNight.LethalCompanyMod.BalancedItems {
@@ -13,28 +12,10 @@ namespace GuysNight.LethalCompanyMod.BalancedItems {
 			SharedComponents.Logger = Logger;
 			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
 			SharedComponents.Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-
-			LoadConfigValues();
-
-			SharedComponents.Logger.LogInfo(ItemOverridesContainer.ItemOverrides.ToDebugString());
-		}
-
-		private void LoadConfigValues() {
 			SharedComponents.ConfigFile = new ConfigFile(Path.Combine(Paths.ConfigPath, $"{PluginInfo.PLUGIN_NAME}.cfg"), true);
 
-			//weights
-			foreach (var (itemName, itemOverrides) in ItemOverridesContainer.ItemOverrides) {
-				//TODO: make this dynamic to where it fetches the displayed item name based on the internal item name
-				itemOverrides.Weight = SharedComponents.ConfigFile.Bind(Constants.ConfigSectionHeaderWeight,
-					itemName,
-					NumericUtilities.DenormalizeWeight(itemOverrides.Weight),
-					string.Format(Constants.ConfigDescriptionWeight, itemName)).Value;
-
-				itemOverrides.AverageValue = SharedComponents.ConfigFile.Bind(Constants.ConfigSectionHeaderAverageSellValues,
-					itemName,
-					itemOverrides.AverageValue,
-					string.Format(Constants.ConfigDescriptionAverageSellValues, itemName)).Value;
-			}
+			SharedComponents.Logger.LogInfo("Default overrides specified in code:");
+			SharedComponents.Logger.LogInfo(ItemOverridesContainer.ItemOverrides.ToDebugString());
 		}
 	}
 }
