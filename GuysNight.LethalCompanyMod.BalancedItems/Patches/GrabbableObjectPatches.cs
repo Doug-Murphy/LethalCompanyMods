76125d3fe9f8dcae4bae32ec9525b6d4f5843d82
Doug Murphy <doug.murphy82@gmail.com>
2024-01-01 19:20:08 -0500

    Create config entry and bind config values to overrides for grabbable items and average sell value when starting a round.

--- GuysNight.LethalCompanyMod.BalancedItems/Patches/GrabbableObjectPatches.cs
+++ GuysNight.LethalCompanyMod.BalancedItems/Patches/GrabbableObjectPatches.cs
@@ -1,5 +1,8 @@
 ﻿#pragma warning disable	S1118
 
+using System;
+using System.Linq;
+using GuysNight.LethalCompanyMod.BalancedItems.Models;
 using GuysNight.LethalCompanyMod.BalancedItems.Utilities;
 using HarmonyLib;
 
@@ -8,7 +11,7 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Patches {
 	public class GrabbableObjectPatches {
 		[HarmonyPatch("Start")]
 		[HarmonyPostfix]
-		public static void ChangeItemWeights(GrabbableObject __instance) {
+		public static void ChangeItemWeight(GrabbableObject __instance) {
 			if (__instance == null) {
 				SharedComponents.Logger.LogInfo("__instance is null for some reason. Exiting override.");
 
@@ -17,14 +20,41 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Patches {
 
 			SharedComponents.Logger.LogInfo($"item.itemProperties.name is '{__instance.itemProperties.name}'");
 			SharedComponents.Logger.LogInfo($"item.itemProperties.weight is '{NumericUtilities.DenormalizeWeight(__instance.itemProperties.weight)}'");
+			SharedComponents.Logger.LogInfo($"item.itemProperties.minValue is '{__instance.itemProperties.minValue}'");
+			SharedComponents.Logger.LogInfo($"item.itemProperties.maxValue is '{__instance.itemProperties.maxValue}'");
 
-			if (!ItemOverridesContainer.ItemOverrides.TryGetValue(__instance.itemProperties.name, out var itemOverride)) {
-				SharedComponents.Logger.LogInfo("No override exists for this item. Making no changes.");
+			SharedComponents.Logger.LogInfo($"Begin adding config entries and setting override values for '{__instance.itemProperties.name}'");
 
-				return;
+			var itemWeight = __instance.itemProperties.weight;
+			var itemMinValue = __instance.itemProperties.minValue;
+			var itemMaxValue = __instance.itemProperties.maxValue;
+			var itemAverageValue = (ushort)Math.Round(new[] { itemMinValue, itemMaxValue }.Average(), MidpointRounding.AwayFromZero);
+
+			//if the overrides container does not contain an override for the current grabbable item, add an entry
+			if (!ItemOverridesContainer.ItemOverrides.ContainsKey(__instance.itemProperties.name)) {
+				ItemOverridesContainer.ItemOverrides[__instance.itemProperties.name] = new OverrideProperties();
 			}
 
-			UpdateItemWeight(__instance, itemOverride.Weight);
+			var itemOverrides = ItemOverridesContainer.ItemOverrides[__instance.itemProperties.name];
+
+			//if weight is not added in the config, add it for future
+			//if weight is added in the config, retrieve the value and set it in the overrides
+			itemOverrides.Weight = NumericUtilities.NormalizeWeight(SharedComponents.ConfigFile.Bind(Constants.ConfigSectionHeaderWeight,
+				__instance.itemProperties.name,
+				NumericUtilities.DenormalizeWeight(itemWeight),
+				string.Format(Constants.ConfigDescriptionWeight, __instance.itemProperties.name)
+			).Value);
+
+			//if sell value is not added in the config, add it for future
+			//if sell value is added in the config, retrieve the value and set it in the overrides
+			itemOverrides.AverageValue = SharedComponents.ConfigFile.Bind(Constants.ConfigSectionHeaderAverageSellValues,
+				__instance.itemProperties.name,
+				itemAverageValue,
+				string.Format(Constants.ConfigDescriptionAverageSellValues, __instance.itemProperties.name)).Value;
+
+			SharedComponents.Logger.LogInfo($"Finish adding config entries and setting override values for '{__instance.itemProperties.name}'");
+
+			UpdateItemWeight(__instance, itemOverrides.Weight);
 		}
 
 		private static void UpdateItemWeight(GrabbableObject item, float weight) {
