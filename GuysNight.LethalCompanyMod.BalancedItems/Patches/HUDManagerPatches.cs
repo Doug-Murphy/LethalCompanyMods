57e30279cfd966da173742f349c9d9b8de09959b
Doug Murphy <doug.murphy82@gmail.com>
2024-01-04 16:20:12 -0500

    Use string interpolation instead of string.Format()

--- GuysNight.LethalCompanyMod.BalancedItems/Patches/HUDManagerPatches.cs
+++ GuysNight.LethalCompanyMod.BalancedItems/Patches/HUDManagerPatches.cs
@@ -11,7 +11,7 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Patches {
 		[HarmonyPostfix]
 		public static void DisplayCorrectCarryWeight(HUDManager __instance) {
 			var num = (float)Math.Round(Math.Clamp(GameNetworkManager.Instance.localPlayerController.carryWeight - 1f, 0.0f, 100f) * 100f);
-			__instance.weightCounter.text = string.Format("{0} lb", num);
+			__instance.weightCounter.text = $"{num} lb";
 		}
 	}
 }
