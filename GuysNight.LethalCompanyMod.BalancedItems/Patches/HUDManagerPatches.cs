ef8e28b668b5d2378c2f2914940d6d938420d7d1
Doug Murphy <doug.murphy82@gmail.com>
2024-01-05 08:57:32 -0500

    Fix null ref in HUDManagerPatches.cs

--- GuysNight.LethalCompanyMod.BalancedItems/Patches/HUDManagerPatches.cs
+++ GuysNight.LethalCompanyMod.BalancedItems/Patches/HUDManagerPatches.cs
@@ -10,6 +10,12 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Patches {
 		[HarmonyPatch("Update")]
 		[HarmonyPostfix]
 		public static void DisplayCorrectCarryWeight(HUDManager __instance) {
+			if (__instance is null) {
+				SharedComponents.Logger.LogInfo($"__instance is null in '{nameof(DisplayCorrectCarryWeight)}'. Aborting.");
+
+				return;
+			}
+
 			var num = (float)Math.Round(Math.Clamp(GameNetworkManager.Instance.localPlayerController.carryWeight - 1f, 0.0f, 100f) * 100f);
 			__instance.weightCounter.text = $"{num} lb";
 		}
