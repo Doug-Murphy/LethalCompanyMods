18d180097e1169c8a9e6e63808df9a462332afe8
Doug Murphy <doug.murphy82@gmail.com>
2024-01-12 20:33:48 -0500

    Strip invalid chars when adding config entries (#19)

--- GuysNight.LethalCompanyMod.BalancedItems/Utilities/ConfigUtilities.cs
+++ GuysNight.LethalCompanyMod.BalancedItems/Utilities/ConfigUtilities.cs
@@ -2,9 +2,40 @@
 using GuysNight.LethalCompanyMod.BalancedItems.Models;
 using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace GuysNight.LethalCompanyMod.BalancedItems.Utilities {
 	public static class ConfigUtilities {
+		/// <summary>
+		/// The list of invalid characters for BepInEx configs. Taken from their decompiled DLL.
+		/// </summary>
+		private static readonly char[] InvalidConfigChars = {
+			'=',
+			'\n',
+			'\t',
+			'\\',
+			'"',
+			'\'',
+			'[',
+			']'
+		};
+
+		//create a regex pattern from the invalid characters. Manually escape ] since it apparently isn't considered special in .NET Standard 2.1.
+		private static readonly string Pattern = "[" + Regex.Escape(new string(InvalidConfigChars)).Replace("]", "\\]") + "]";
+
+		//use that pattern to compile a Regex replacement.
+		private static readonly Regex InvalidConfigCharsRegex = new Regex(Pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+		/// <summary>
+		/// Sanitize the config entry so that it is allowed by BepInEx
+		/// </summary>
+		/// <param name="rawEntry">The entry to sanitize.</param>
+		/// <returns></returns>
+		private static string SanitizeConfigEntry(string rawEntry) {
+			//replace invalid characters with '_'
+			return InvalidConfigCharsRegex.Replace(rawEntry, "_");
+		}
+
 		public static void SyncConfigForItemOverrides(Item item, out OverrideProperties overrideProperties) {
 			var itemWeight = item.weight;
 			var itemMinValue = item.minValue;
@@ -21,16 +52,16 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Utilities {
 			SharedComponents.ConfigFile.Reload();
 			//if weight is not added in the config, add it for future
 			//if weight is added in the config, retrieve the value and set it in the overrides
-			itemOverrides.Weight = SharedComponents.ConfigFile.Bind(Constants.ConfigSectionHeaderWeight,
-				item.name,
+			itemOverrides.Weight = SharedComponents.ConfigFile.Bind(SanitizeConfigEntry(Constants.ConfigSectionHeaderWeight),
+				SanitizeConfigEntry(item.name),
 				NumericUtilities.DenormalizeWeight(Math.Abs(itemOverrides.Weight - default(float)) > 0 ? itemOverrides.Weight : itemWeight),
 				new ConfigDescription(string.Format(Constants.ConfigDescriptionWeight, item.itemName), new AcceptableValueRange<float>(0, 1_000))
 			).Value;
 
 			//if sell value is not added in the config, add it for future
 			//if sell value is added in the config, retrieve the value and set it in the overrides
-			itemOverrides.AverageValue = SharedComponents.ConfigFile.Bind(Constants.ConfigSectionHeaderAverageSellValues,
-				item.name,
+			itemOverrides.AverageValue = SharedComponents.ConfigFile.Bind(SanitizeConfigEntry(Constants.ConfigSectionHeaderAverageSellValues),
+				SanitizeConfigEntry(item.name),
 				itemOverrides.AverageValue != default ? itemOverrides.AverageValue : itemAverageValue,
 				new ConfigDescription(string.Format(Constants.ConfigDescriptionAverageSellValues, item.itemName), new AcceptableValueRange<ushort>(ushort.MinValue, ushort.MaxValue))
 			).Value;
@@ -55,8 +86,8 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Utilities {
 			SharedComponents.ConfigFile.Reload();
 			//if rarity for the moon is not added in the config, add it for future
 			//if it is added in the config, retrieve the value and set it in the overrides
-			itemOverrides.MoonRarities[level.name] = SharedComponents.ConfigFile.Bind(string.Format(Constants.ConfigSectionHeaderMoonRarity, level.PlanetName),
-				item.name,
+			itemOverrides.MoonRarities[level.name] = SharedComponents.ConfigFile.Bind(SanitizeConfigEntry(string.Format(Constants.ConfigSectionHeaderMoonRarity, level.PlanetName)),
+				SanitizeConfigEntry(item.name),
 				itemOverrides.MoonRarities[level.name].HasValue ? itemOverrides.MoonRarities[level.name].Value : (byte)itemRarity,
 				new ConfigDescription(string.Format(Constants.ConfigDescriptionMoonRarity, item.itemName), new AcceptableValueRange<byte>(0, 100)) //100 is the max in the game
 			).Value;
