82ffef0f9a10873b2e3b2351667fb54ac258daa4
Doug Murphy <doug.murphy82@gmail.com>
2024-01-17 16:49:54 -0500

    Make the bridge invincible!

---
+++ GuysNight.LethalCompanyMod.InvincibleBridge/Patches/BridgeTriggerPatches.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace GuysNight.LethalCompanyMod.InvincibleBridge.Patches {
+	[HarmonyPatch(typeof(BridgeTrigger))]
+	public class BridgeTriggerPatches {
+		[HarmonyPatch("Update")]
+		[HarmonyPrefix]
+		public static void SetBridgeDurabilityToOne(BridgeTrigger __instance) {
+			__instance.bridgeDurability = 1.0f;
+		}
+	}
+}
