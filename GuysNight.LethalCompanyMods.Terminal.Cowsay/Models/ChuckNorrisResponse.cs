0425a320248f6e1a631102fd5c391dd99b5656ae
Doug Murphy <doug.murphy82@gmail.com>
2024-01-18 14:55:45 -0500

    Implement "cowsay chuck" command in terminal.

---
+++ GuysNight.LethalCompanyMods.Terminal.Cowsay/Models/ChuckNorrisResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+#pragma warning disable S1104
+#pragma warning disable CS0649
+#pragma warning disable CS8618
+namespace GuysNight.LethalCompanyMods.Terminal.Cowsay.Models {
+	[Serializable]
+	public class ChuckNorrisResponse {
+		public string[] categories;
+
+		public DateTime created_at;
+
+		public string icon_url;
+
+		public string id;
+
+		public DateTime updated_at;
+
+		public string url;
+
+		public string value;
+	}
+}
--- GuysNight.LethalCompanyMods.Terminal.Cowsay/Plugin.cs
+++ GuysNight.LethalCompanyMods.Terminal.Cowsay/Plugin.cs
@@ -1,31 +1,63 @@
-﻿using BepInEx;
+﻿#pragma warning disable S1075
+using BepInEx;
+using GuysNight.LethalCompanyMods.Terminal.Cowsay.Models;
 using HarmonyLib;
+using System.Net.Http;
 using System.Reflection;
+using System.Web;
 using TerminalApi.Classes;
+using UnityEngine;
 using static TerminalApi.TerminalApi;
 
 namespace GuysNight.LethalCompanyMods.Terminal.Cowsay {
 	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 	[BepInDependency("atomic.terminalapi", "1.5.0")]
 	public class Plugin : BaseUnityPlugin {
+		private const string ChuckNorrisApiUrl = "https://api.chucknorris.io/jokes/random";
+		private const string CowsayApiUrl = "https://cowsay.morecode.org/say";
+		private static readonly HttpClient _httpClient = new HttpClient();
+
 		private void Awake() {
 			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
-			// Adds time command, 'check' is the verb. Verbs are optional
-			AddCommand("cowsay-chuck", new CommandInfo {
+			AddCommand("chuck", new CommandInfo {
 					Category = "other",
-					Description = "Displays the current time.",
-					DisplayTextSupplier = OnTimeCommand
+					Description = "Displays a cow telling you a random Chuck Norris joke.",
+					DisplayTextSupplier = GetCowsayChuckNorris,
+					Title = "Cowsay"
 				},
-				"check");
-			AddCommand("cowsay", new CommandInfo {
-					Category = "other",
-					Description = "Displays the current time.",
-					DisplayTextSupplier = OnTimeCommand
-				},
-				"check");
+				"cowsay");
+		}
 
-			//cowsay -n hello, world. I am a cow!
-			//cowsay hello, world. I am a cow!
+		private string GetCowsayChuckNorris() {
+			var chuckNorrisResponse = _httpClient.GetAsync(ChuckNorrisApiUrl).GetAwaiter().GetResult();
+
+			if (!chuckNorrisResponse.IsSuccessStatusCode) {
+				Logger.LogWarning($"Chuck Norris API did not return a successful status code. Returned {chuckNorrisResponse.StatusCode} with content {chuckNorrisResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult()}");
+
+				return "An error occurred. Please try again later.";
+			}
+
+			var serializedChuckNorrisResponse = chuckNorrisResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+			var deserializedChuckNorrisResponse = JsonUtility.FromJson<ChuckNorrisResponse>(serializedChuckNorrisResponse);
+			Logger.LogDebug($"Chuck Norris API response is '{serializedChuckNorrisResponse}'");
+			if (deserializedChuckNorrisResponse is null) {
+				Logger.LogWarning($"Chuck Norris response could not be deserialized. The response was {serializedChuckNorrisResponse}");
+
+				return "An error occurred. Please try again later.";
+			}
+
+			Logger.LogDebug($"Chuck Norris API deserialized response's value property is '{deserializedChuckNorrisResponse.value}'");
+
+
+			var cowsayRequest = _httpClient.GetAsync($"{CowsayApiUrl}?format=text&message={HttpUtility.UrlEncode(deserializedChuckNorrisResponse.value)}").GetAwaiter().GetResult();
+
+			if (!cowsayRequest.IsSuccessStatusCode) {
+				Logger.LogWarning($"Cowsay API did not return a successful status code. Returned {cowsayRequest.StatusCode} with content {cowsayRequest.Content.ReadAsStringAsync().GetAwaiter().GetResult()}");
+
+				return "An error occurred. Please try again later.";
+			}
+
+			return cowsayRequest.Content.ReadAsStringAsync().GetAwaiter().GetResult();
 		}
 	}
 }
