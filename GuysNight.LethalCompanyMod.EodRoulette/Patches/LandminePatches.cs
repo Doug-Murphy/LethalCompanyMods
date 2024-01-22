67a3dc9f6526c124229effd72b634aa65827b7c8
Doug Murphy <doug.murphy82@gmail.com>
2024-01-22 16:24:16 -0500

    Enable config support to set the chance to disable. Default is 69%. Lol.

--- GuysNight.LethalCompanyMod.EodRoulette/Patches/LandminePatches.cs
+++ GuysNight.LethalCompanyMod.EodRoulette/Patches/LandminePatches.cs
@@ -1,6 +1,5 @@
 ﻿#pragma warning disable S1118
 
-using BepInEx.Logging;
 using HarmonyLib;
 using System;
 
@@ -8,23 +7,29 @@ namespace GuysNight.LethalCompanyMod.EodRoulette.Patches {
 	[HarmonyPatch(typeof(Landmine))]
 	public class LandminePatches {
 		private static readonly Random RandomGenerator = new Random();
-		private static readonly ManualLogSource Logger = BepInEx.Logging.Logger.CreateLogSource(PluginInfo.PLUGIN_NAME);
 
 		[HarmonyPatch("TriggerMineOnLocalClientByExiting")]
 		[HarmonyPrefix]
 		public static void PotentiallyAvoidExplosion(Landmine __instance) {
-			var randomNumber = RandomGenerator.Next(1, 101); //generate random int between 1 and 100, inclusive
+			if (byte.TryParse(SharedComponents.ConfigFile[Constants.ConfigSectionHeader, Constants.ConfigChanceToDisableEntryKey].GetSerializedValue(), out var chanceToDisable)) {
+				SharedComponents.Logger.LogDebug($"Successfully retrieved chance to disable. Value is '{chanceToDisable}'");
+			}
+			else {
+				SharedComponents.Logger.LogWarning($"Could not retrieve chance to disable from config. Assuming it was set to the default value of {Constants.DefaultChanceToDisable}.");
+				chanceToDisable = Constants.DefaultChanceToDisable;
+			}
 
-			Logger.LogDebug($"Random number generated {randomNumber}");
-			if (randomNumber < 90) {
+			var randomNumber = RandomGenerator.Next(1, 101); //generate random int between 1 and 100, inclusive
+			SharedComponents.Logger.LogDebug($"Random number generated {randomNumber}");
+			if (randomNumber > chanceToDisable) {
 				//the random number generation landed within the range of FAILURE. So let the normal event occur
-				Logger.LogDebug("The number generated resulted in a failed defusal. Letting game's code execute.");
+				SharedComponents.Logger.LogDebug("The number generated resulted in a failed defusal. Letting game's code execute.");
 
 				return;
 			}
 
 			// the random generation landed within the range of a success
-			Logger.LogDebug("The number generated resulted in a successful defusal. Doing overrides.");
+			SharedComponents.Logger.LogDebug("The number generated resulted in a successful defusal. Doing overrides.");
 
 			//stop the landmine red light from blinking and stop the sound it makes
 			__instance.mineAnimator.enabled = false;
--- GuysNight.LethalCompanyMod.EodRoulette/Plugin.cs
+++ GuysNight.LethalCompanyMod.EodRoulette/Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using System.Reflection;
 
@@ -7,6 +8,11 @@ namespace GuysNight.LethalCompanyMod.EodRoulette {
 	public class Plugin : BaseUnityPlugin {
 		private void Awake() {
 			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+			SharedComponents.ConfigFile.Bind("Settings",
+				"ChanceToDisable",
+				Constants.DefaultChanceToDisable,
+				new ConfigDescription("The percentage chance that the mine will be disabled when stepping off of it. This value can be set anytime and the updated value will be respected upon stepping off of the mine.", new AcceptableValueRange<byte>(0, 100)));
 		}
 	}
 }
