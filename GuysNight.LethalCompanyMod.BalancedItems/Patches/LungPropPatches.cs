966653bb806d74e9991141d47ec89515db23f483
Doug Murphy <doug.murphy82@gmail.com>
2024-01-25 08:34:53 -0500

    Set apparatus value to override range.

---
+++ GuysNight.LethalCompanyMod.BalancedItems/Patches/LungPropPatches.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System;
+
+namespace GuysNight.LethalCompanyMod.BalancedItems.Patches {
+	[HarmonyPatch(typeof(LungProp))]
+	public class LungPropPatches {
+		private static readonly Random RandomGenerator = new Random();
+
+		[HarmonyPatch("DisconnectFromMachinery")]
+		[HarmonyPrefix]
+		public static void SetApparatusValue(LungProp __instance) {
+			if (!ItemsContainer.Items.TryGetValue(__instance.itemProperties.name, out var itemEntry)) {
+				//should be impossible so long we initialize the collection in StartOfRoundPatches
+				SharedComponents.Logger.LogWarning($"No item entry exists for item '{__instance.itemProperties.name}'. Making no changes to item value.");
+
+				return;
+			}
+
+			if (bool.TryParse(SharedComponents.ConfigFile[Constants.ConfigSectionHeaderToggles, Constants.ConfigKeyToggleAverageSellValues].GetSerializedValue(), out var isSellValueFeatureEnabled)) {
+				SharedComponents.Logger.LogDebug($"Successfully retrieved sell value override feature toggle. Value is '{isSellValueFeatureEnabled}'");
+			}
+			else {
+				SharedComponents.Logger.LogWarning("Could not retrieve sell value override feature toggle from config. Assuming it was set to true.");
+				isSellValueFeatureEnabled = true;
+			}
+
+			if (!isSellValueFeatureEnabled) {
+				return;
+			}
+
+			var randomValue = RandomGenerator.Next(itemEntry.Overrides.MinValue, itemEntry.Overrides.MaxValue + 1);
+
+			__instance.SetScrapValue(randomValue);
+		}
+	}
+}
