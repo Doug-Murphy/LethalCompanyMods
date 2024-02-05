289e910b6c0c7c63eb08c0d918cc1805656737c1
Doug Murphy <doug.murphy82@gmail.com>
2024-02-05 08:34:38 -0500

    Remove commented code from ship decor progress.

--- GuysNight.LethalCompanyMod.BalancedItems/Patches/TerminalPatches.cs
+++ GuysNight.LethalCompanyMod.BalancedItems/Patches/TerminalPatches.cs
@@ -9,22 +9,7 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Patches {
 			SharedComponents.ConfigFile.Reload();
 
 			SetItemPricesInShop(__instance);
-			// SetShipUpgradePricesInShop();
 		}
-
-		// [HarmonyPatch("LoadNewNodeIfAffordable")]
-		// [HarmonyPrefix]
-		// public static void LoadNewNodePrefix(TerminalNode node) {
-		// 	switch (node.name) {
-		// 		case "LoudHornBuy1":
-		// 		case "LoudHornBuyConfirm":
-		// 			node.itemCost = 5;
-		//
-		// 			break;
-		// 	}
-		//
-		// 	SharedComponents.Logger.LogDebug($"node.name = '{node.name}' node.itemCost = '{node.itemCost}' node.displayText = '{node.displayText}");
-		// }
 
 		private static void SetItemPricesInShop(Terminal __instance) {
 			foreach (var buyableItem in __instance.buyableItemsList) {
@@ -33,14 +18,5 @@ namespace GuysNight.LethalCompanyMod.BalancedItems.Patches {
 				SharedComponents.Logger.LogDebug($"Set buyable item '{buyableItem.itemName}' creditsWorth to '{buyableItem.creditsWorth}'");
 			}
 		}
-
-		// private static void SetShipUpgradePricesInShop() {
-		// 	foreach (var shipUpgrade in StartOfRound.Instance.unlockablesList.unlockables) {
-		// 		SharedComponents.Logger.LogDebug($"shipUpgrade stuff -> '{shipUpgrade.unlockableName}', {shipUpgrade.unlockableType}");
-		// 		Unlockables.RegisterUnlockable(shipUpgrade, 0, StoreType.ShipUpgrade);
-		// 		Unlockables.UpdateUnlockablePrice(shipUpgrade, 5);
-		// 		SharedComponents.Logger.LogDebug($"Found ship upgrade unlockableName '{shipUpgrade.unlockableName}'");
-		// 	}
-		// }
 	}
 }
